c32e5bc4c0d428e7f84d462b4d8922753b9c7f76
Sanhar-Bala-Finix <sanhar.bala@finix.com>
2025-12-04 13:06:05 -0500

    CP-1672: Update Library Version to 3.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
-    implementation("com.finix:pax-mpos-sdk-android:3.3.4")
+    implementation("com.finix:pax-mpos-sdk-android:3.4.0")
 
 }
